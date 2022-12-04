6e9d32dac4d9d9c04963b54ea9e88d297a7ce9a4
Willian Silva <will.devfrontend@gmail.com>
2022-12-04 19:09:42 -0300

    feat: :sparkles: create redirect on select

--- src/pages/Cobertura/index.js
+++ src/pages/Cobertura/index.js
@@ -1,11 +1,27 @@
-import { useState } from 'react';
-import './Cobertura.css';
-export default function Cobertura(){
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import "./Cobertura.css";
+export default function Cobertura() {
+    const { register, getValues } = useForm();
+    const navigate = useNavigate();
 
-    return (
-<>
-
-</>
-    )
+  return (
+    <>
+      <select {...register("cidade")}>
+        <option value="0">Teste</option>
+              <option value="1" >Teste1</option>
+          </select>
+          
+          <button type="submit" onClick={() => {
+              const values = getValues()
+              console.log(values)
+              if (values.cidade === "1") {
+                navigate("/lite")
+              } else {
+                  navigate("/fibra")
+              }
+        }}>Enviar</button>
+    </>
+  );
 }
- 
